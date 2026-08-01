283178f52607574afbbab844aac4f511aed4ab8f
Abel Nagy <abel.nagy.m4tankor@gmail.com>
2026-08-01 19:20:31 +0200

    Initialize repo.

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string_view>
+#include <concepts>
+static_assert(std::integral<int>);
+
+int main() {
+    constexpr std::string_view msg = "hack assembler v0 - C++20 OK";
+    std::cout << msg << '\n';
+    return 0;
+}
